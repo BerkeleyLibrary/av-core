f74fc35f4abfe331068fdcdb8f74a3c7a9fc5a9b
David Moles <dmoles@dmoles.net>
2019-11-26 11:20:31 -0800

    Fix gem name and metadata

--- lib/av_core/module_info.rb
+++ lib/av_core/module_info.rb
@@ -1,12 +1,12 @@
 module AVCore
   class ModuleInfo
-    NAME = 'avplayer_core'.freeze
+    NAME = 'av_core'.freeze
     AUTHOR = 'David Moles'.freeze
     AUTHOR_EMAIL = 'dmoles@berkeley.edu'.freeze
-    SUMMARY = 'UC Berkeley Library Audio/Video Player core code'.freeze
-    DESCRIPTION = 'Gem for audio/video playback at the UC Berkeley Library'.freeze
+    SUMMARY = 'UC Berkeley Library audio/video core code'.freeze
+    DESCRIPTION = 'Gem for UC Berkeley Library shared audio/video code'.freeze
     LICENSE = 'MIT'.freeze
     VERSION = '0.0.1'.freeze
-    HOMEPAGE = 'https://git.lib.berkeley.edu/lap/avplayer_core'.freeze
+    HOMEPAGE = 'https://git.lib.berkeley.edu/lap/av_core'.freeze
   end
 end
