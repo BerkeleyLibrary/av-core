3c9c5c560f1eec3f5ade178a2ea3c1daebbda9c6
David Moles <dmoles@berkeley.edu>
2020-04-21 11:34:58 -0700

    Add some utility methods for reading MARC files

--- lib/av/marc.rb
+++ lib/av/marc.rb
@@ -20,6 +20,31 @@ module AV
         input = StringIO.new(xml.scrub)
         MARC::XMLReader.new(input)
       end
+
+      # Returns a reader for the specified MARC file.
+      #
+      # @param marc_path [String] the path to a MARC file in XML or binary format
+      # @param external_encoding [String] the encoding, for binary files
+      # @return [MARC::XMLReader] if the file path ends in `.xml`
+      # @return [MARC::Reader] if the file path ends in `.mrc`
+      def reader_for(marc_path, external_encoding: 'MARC-8')
+        downcased_path = marc_path.downcase
+        return MARC::XMLReader.new(marc_path) if downcased_path.end_with?('.xml')
+        return MARC::Reader.new(marc_path, external_encoding: external_encoding) if downcased_path.end_with?('.mrc')
+
+        raise ArgumentError, "Unable to determine reader needed for MARC file #{marc_path.inspect}"
+      end
+
+      # Returns a MARC record read from the specified file (or the first record
+      # if the file contains multiple records).
+      #
+      # @param marc_path [String] the path to a MARC file in XML or binary format
+      # @param external_encoding [String] the encoding, for binary files
+      # @return [MARC::Record] the MARC record
+      def read(marc_path, external_encoding: 'MARC-8')
+        # noinspection RubyYardReturnMatch
+        reader_for(marc_path, external_encoding: external_encoding).first
+      end
     end
   end
 end
--- spec/lib/av/marc_spec.rb
+++ spec/lib/av/marc_spec.rb
@@ -21,5 +21,40 @@ module AV
         end
       end
     end
+
+    describe :reader_for do
+      it 'returns a reader for XML' do
+        marc_path = 'spec/data/record-(cityarts)00002.xml'
+        reader = Marc.reader_for(marc_path)
+        record = reader.first
+        expect(record['245'].value).to eq('826 Spelling Bee For Cheaters, February 17, 2011')
+      end
+
+      it 'returns a reader for binary MARC' do
+        marc_path = 'spec/data/10.23.19.JessieLaCavalier.02.mrc'
+        reader = Marc.reader_for(marc_path)
+        record = reader.first
+        expect(record['100'].value).to eq('LeCavalier, Jesse.')
+      end
+
+      it 'raises an error for non-MARC files' do
+        marc_path = 'I am not a MARC file'
+        expect { Marc.reader_for(marc_path) }.to raise_error(ArgumentError)
+      end
+    end
+
+    describe :read do
+      it 'reads XML' do
+        marc_path = 'spec/data/record-(cityarts)00002.xml'
+        record = Marc.read(marc_path)
+        expect(record['245'].value).to eq('826 Spelling Bee For Cheaters, February 17, 2011')
+      end
+
+      it 'reads binary MARC' do
+        marc_path = 'spec/data/10.23.19.JessieLaCavalier.02.mrc'
+        record = Marc.read(marc_path)
+        expect(record['100'].value).to eq('LeCavalier, Jesse.')
+      end
+    end
   end
 end
