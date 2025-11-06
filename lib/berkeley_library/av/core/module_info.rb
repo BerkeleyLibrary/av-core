9ef2aaaaa8b6e7e9251fc2aa107c187693e51fd4
María A. Matienzo <matienzo@berkeley.edu>
2025-11-06 10:10:07 -0800

    prep for 0.5.0 release

--- lib/berkeley_library/av/core/module_info.rb
+++ lib/berkeley_library/av/core/module_info.rb
@@ -3,12 +3,12 @@ module BerkeleyLibrary
     module Core
       class ModuleInfo
         NAME = 'berkeley_library-av-core'.freeze
-        AUTHOR = 'David Moles'.freeze
-        AUTHOR_EMAIL = 'dmoles@berkeley.edu'.freeze
+        AUTHORS = ['David Moles', 'maría a. matienzo'].freeze
+        AUTHOR_EMAILS = ['dmoles@berkeley.edu', 'matienzo@berkeley.edu'].freeze
         SUMMARY = 'UC Berkeley Library audio/video core code'.freeze
         DESCRIPTION = 'Gem for UC Berkeley Library shared audio/video code'.freeze
         LICENSE = 'MIT'.freeze
-        VERSION = '0.4.3'.freeze
+        VERSION = '0.5.0'.freeze
         HOMEPAGE = 'https://github.com/BerkeleyLibrary/av-core'.freeze
 
         private_class_method :new
